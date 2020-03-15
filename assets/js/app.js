42501a2bf0567bf2532024f1db06ec5b5bf69965
Alex Rocha <alex.rocha@zalando.de>
2020-03-15 12:28:36 +0100

    feat: add text and css formatting for about me

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,40 +1,55 @@
 (function($) {
 
- function init() {
-    /* Sidebar height set */
-    $sidebarStyles = $('.sidebar').attr('style')
-    $sidebarStyles += ' min-height: ' + $(document).height() + 'px;';
-    $('.sidebar').attr('style', $sidebarStyles);
+    function init() {
+        /* Sidebar height set */
+        $sidebarStyles = $('.sidebar').attr('style')
+        $sidebarStyles += ' min-height: ' + $(document).height() + 'px;';
+        $('.sidebar').attr('style', $sidebarStyles);
 
-    /* Secondary contact links */
-    var $scontacts = $('#contact-list-secondary');
-    var $contactList = $('#contact-list');
+        /* Secondary contact links */
+        var $scontacts = $('#contact-list-secondary');
+        var $contactList = $('#contact-list');
 
-    $scontacts.hide();
-    $contactList.mouseenter(function(){ $scontacts.fadeIn(); });
-    $contactList.mouseleave(function(){ $scontacts.fadeOut(); });
+        $scontacts.hide();
+        $contactList.mouseenter(function(){ $scontacts.fadeIn(); });
+        $contactList.mouseleave(function(){ $scontacts.fadeOut(); });
 
-    /**
-     * Tags & categories tab activation based on hash value. If hash is undefined then first tab is activated.
-     */
-    function activateTab() {
-      if(['/tags.html', '/categories.html'].indexOf(window.location.pathname) > -1) {
-        var hash = window.location.hash;
-        if(hash)
-          $('.tab-pane').length && $('a[href="' + hash + '"]').tab('show');
-        else
-          $('.tab-pane').length && $($('.cat-tag-menu li a')[0]).tab('show');
-      }
+        /**
+         * Tags & categories tab activation based on hash value. If hash is undefined then first tab is activated.
+         */
+        function activateTab() {
+            if(['/tags.html', '/categories.html'].indexOf(window.location.pathname) > -1) {
+                var hash = window.location.hash;
+                if(hash)
+                    $('.tab-pane').length && $('a[href="' + hash + '"]').tab('show');
+                else
+                    $('.tab-pane').length && $($('.cat-tag-menu li a')[0]).tab('show');
+            }
+        }
+
+        // watch hash change and activate relevant tab
+        $(window).on('hashchange', activateTab);
+
+        // initial activation
+        activateTab();
     }
 
-    // watch hash change and activate relevant tab
-    $(window).on('hashchange', activateTab);
+    // run init on document ready
+    $(document).ready(init);
 
-    // initial activation
-    activateTab();
-  };
-
-  // run init on document ready
-  $(document).ready(init);
+    $('.about-me-link').click(function() {
+        $(document.getElementsByClassName('sidebar')[0]).click();
+    })
 
 })(jQuery);
+
+function showAboutMe() {
+    let sidebar = $(document.getElementsByClassName('sidebar')[0]);
+    sidebar.addClass('about-me-fs');
+    sidebar.removeClass('sidebar-hover');
+
+    $(document.getElementById('contact-list')).addClass('contact-list-fs');
+
+    let aboutMeDiv = $(document.getElementById('about-me'));
+    aboutMeDiv.fadeIn(3000);
+}
